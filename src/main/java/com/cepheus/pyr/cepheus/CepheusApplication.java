49eb5bd9d0f71fe7a710e87a048b1973e749c6d5
rafael31p <rafael31p@gmail.com>
2024-05-01 13:30:28 -0500

    Se realiza primer commit proyecto CepheusApplication en relacion en un proceso de arquitectura exagonal

---
+++ src/main/java/com/cepheus/pyr/cepheus/CepheusApplication.java
@@ -0,0 +1,13 @@
+package com.cepheus.pyr.cepheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepheusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cepheus/pyr/cepheus/CepheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cepheus.pyr.cepheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
